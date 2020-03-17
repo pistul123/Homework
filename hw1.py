d821a216cc8a1a8358043b026ca0609ef9ca4f67
pistul123 <62251594+pistul123@users.noreply.github.com>
2020-03-17 20:57:42 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -54,8 +54,9 @@ def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     year_fix = year - 2000
     grouped = confirmed_cases.groupby("Country/Region", as_index=False).sum(level=0)
     sort_all = grouped.sort_values(by=[f"{month}/{day}/{year_fix}"], ascending = False)
-    top5_countries = sort_all["Country/Region"].head(5).tolist()
-    return top5_countries
+    top5_countries = sort_all["Country/Region"].tolist()
+    mielonka = top5_countries[:5]
+    return mielonka
 
 
 # Function name is wrong, read the pydoc
